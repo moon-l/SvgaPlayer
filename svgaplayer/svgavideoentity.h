55145996f170aa5e517bde85ca772387884e0d88
Moon <todd.linz@gmail.com>
2019-05-31 13:33:40 +0800

    svga资源加载优化

--- svgaplayer/svgavideoentity.h
+++ svgaplayer/svgavideoentity.h
@@ -26,22 +26,30 @@ public:
 
 	bool parse(Json::Value& jsonObj);
 	bool parse(const com::opensource::svga::MovieEntity& obj);
+	bool parseImage(const std::string& buffer, const QString& name);
 	void clear();
+
+	bool valid();
 
 	QString version();
 	int width();
 	int height();
 	int fps();
 	int frames();
+	QPixmap getImage(const QString& key);
 
 	QVector<SvgaVideoSpriteEntity*>& sprites();
 
 private:
+	bool	m_bValid;
+
 	QString	m_version;
 	int		m_width;
 	int		m_height;
 	int		m_fps;
 	int		m_frames;
+
+	QMap<QString, QPixmap>	m_images;
 
 	QVector<SvgaVideoSpriteEntity*>	m_sprites;
 };
